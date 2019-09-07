461cbded04a8f7d69c0568276bd05d76893006b7
user <user@user-PC.mshome.net>
2019-09-07 12:56:02 +0530

    Test Case Created

---
+++ Object Repository/Google_search_object/search_input.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>search_input</name>
+   <tag></tag>
+   <elementGuidId>70e8a9da-5616-449d-97dd-2034b108bda4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;tsf&quot;]/div[2]/div[1]/div[1]/div/div[2]/input</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Scripts/Google_Search_Test/Test_Case_Search/Script1567840310922.groovy
@@ -0,0 +1,31 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('https://www.google.com/')
+
+for (def index : (1..3)) {
+    WebUI.setText(findTestObject('Google_search_object/search_input'), findTestData('Test_data_google_search').getValue(
+            1, index))
+
+    WebUI.delay(2)
+
+    WebUI.sendKeys(findTestObject('Google_search_object/search_input'), Keys.chord(Keys.ENTER))
+
+    WebUI.delay(5)
+
+    not_run: WebUI.navigateToUrl('https://www.google.com/')
+}
+
